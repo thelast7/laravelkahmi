454de226985454da0c9243b0026bea580fd56729
thelast7 <takimanwar@gmail.com>
2018-02-19 13:55:29 +0700

    takim commiet

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>KAHMIPRENEUR | Home</title>
+  <title>KAHMIPRENEUR @yield('title')| HOME</title>
 
   <meta charset="utf-8">
   <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
@@ -42,7 +42,92 @@
   </div>
 
 	<!-- Partial Header -->
-	@include('layouts.partials.header')
+	  <header class="nav-type-2">
+    
+    <nav class="navbar navbar-static-top">
+      <div class="navigation">
+        <div class="container relative">
+
+          <form method="get" class="search-wrap">
+            <input type="search" class="form-control" placeholder="Type &amp; Hit Enter">
+          </form>
+
+          <div class="row">
+
+            <div class="navbar-header">
+              <!-- Logo -->
+              <div class="logo-container">
+                <div class="logo-wrap">
+                  <a href="welcome">
+                    <img class="logo" src="img/logo_dark.png" alt="logo">
+                  </a>
+                </div>
+              </div>
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+            </div> <!-- end navbar-header -->
+
+            <div class="col-md-9 nav-wrap right">
+              
+              <div class="collapse navbar-collapse" id="navbar-collapse">
+                
+                <ul class="nav navbar-nav navbar-right">
+
+                  <li class="dropdown active">
+                    <a href="{{ url('/') }}">HOME</a>
+                  </li>
+
+                  <li class="dropdown">
+                    <a href="{{ url('about') }}">KAHMIPRENEUR</a>
+                  </li>
+
+                  <li class="dropdown">
+                    <a href="{{ url('movie') }}">VIDEO</a>
+                  </li>
+
+                  <li class="dropdown">
+                    <a href="{{ url('berita') }}">BERITA</a>
+                  </li>
+
+                  <li class="dropdown">
+                    <a href="{{ url('forum') }}">FORUM</a>
+                  </li>
+
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle">PENDAFTARAN</a>
+                    <ul class="dropdown-menu menu-right">
+                      <li><a href="{{ route('login') }}">Log in</a></li>
+                      <li><a href="{{ route('register') }}">Sign Up</a></li>
+                    </ul>
+                  </li>
+
+              <li id="mobile-search">
+                    <form method="get" class="mobile-search">
+                      <input type="search" class="form-control" placeholder="Search...">
+                      <button type="submit" class="search-button">
+                        <i class="fa fa-search"></i>
+                      </button>
+                    </form>
+                  </li>
+
+                  <li>
+                    <a href="#" class="nav-search">
+                      <i class="fa fa-search search-trigger"></i>
+                      <i class="fa fa-times search-close"></i>
+                    </a>
+                  </li> 
+              </div> <!-- end collapse -->
+            </div> <!-- end col -->
+            
+          </div> <!-- end row -->
+        </div> <!-- end container -->
+      </div> <!-- end navigation -->
+    </nav> <!-- end navbar -->
+  </header>
 
   <div class="main-wrapper-mp oh">
 
@@ -64,12 +149,12 @@
   </div> <!-- end main-wrapper -->
   
   <!-- jQuery Scripts -->
-  <script type="text/javascript" src="js/jquery.min.js"></script>
-  <script type="text/javascript" src="js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="js/plugins.js"></script>
-  <script type="text/javascript" src="revolution/js/jquery.themepunch.tools.min.js"></script>
-  <script type="text/javascript" src="revolution/js/jquery.themepunch.revolution.min.js"></script>
-  <script type="text/javascript" src="js/rev-slider.js"></script>
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/plugins.js"></script>
+    <script type="text/javascript" src="revolution/js/jquery.themepunch.tools.min.js"></script>
+    <script type="text/javascript" src="revolution/js/jquery.themepunch.revolution.min.js"></script>
+    <script type="text/javascript" src="js/rev-slider.js"></script>
     <script type="text/javascript" src="js/scripts.js"></script>
 
 
