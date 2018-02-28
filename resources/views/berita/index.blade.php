fa396ff7b68c660d4e0b9200521dcebe7a9d0da1
thelast7 <takimanwar@gmail.com>
2018-02-28 14:13:21 +0700

    perubahan lagi

--- resources/views/berita/index.blade.php
+++ resources/views/berita/index.blade.php
@@ -69,119 +69,6 @@
               </div>
             </div> <!-- end entry item -->
             @endforeach
-            <!-- gallery post -->
-            <!-- <div class="entry-item">
-              <div class="entry-slider">
-                <div class="flexslider" id="one-img-slide">
-                  <ul class="slides">
-                    <li>
-                      <a href="#">
-                        <img src="img/blog/gallery_post_img_1.jpg" alt="">
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <img src="img/blog/gallery_post_img_2.jpg" alt="">
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <img src="img/blog/gallery_post_img_3.jpg" alt="">
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div> --> <!-- end slider -->
-
-              <!-- <div class="entry-date hidden-sm hidden-xs">
-                <span>11</span>
-                <span>MAR</span>                    
-              </div>
-
-              <div class="entry-title">
-                <h2>
-                  <a href="{{ url('isiberita') }}">Ini adalah Judul dari beritanya.</a>
-                </h2>
-              </div>
-              <ul class="entry-meta">
-                <li class="entry-date">
-                  <a href="#">11 March, 2018</a>
-                </li>
-                <li class="entry-author">
-                  by <a href="#">Iqbal Buchori</a>
-                </li>
-                <li class="entry-category">
-                  in <a href="#">Programmer</a>
-                </li>
-                <li>
-                  <a href="{{ url('isiberita') }}" class="entry-comments">69 Comments</a>
-                </li>               
-              </ul>
-
-              <div class="entry">         
-
-                <div class="entry-content">
-                  Wakil Gubernur DKI Jakarta Sandiaga Salahuddin Uno mengisi kuliah umum atau studium generale bertema "Being A Winning Young Entrepreneur" di Fakultas Agama Islam, Universitas Muhammadiyah Prof. Dr. Hamka (Uhamka), Jakarta Selata. Dalam kesempatan ini, Sandi menyampaikan mahasiswa pasca-lulus meraih gelar sarjana dapat membuka lapangan pekerjaan melalui wirausaha daripada mencari lapangan pekerjaan..
-                  <br>
-                  <a href="{{ url('isiberita') }}" class="read-more">Selengkapnya</a>
-                  <i class="icon arrow_right"></i>
-                </div>
-              </div>
-            </div> --> <!-- end entry item -->
-
-            <!--  blockquote post
-            	<div class="entry-item"> 
-              <div class="entry blockquote mt-0">             
-                <blockquote class="blockquote-style-1 text-center mb-0">
-                  <i class="fa fa-quote-left"></i>
-                  <p>
-                    <a href="{{ url('isiberita') }}">Ini adalah Judul dari beritanya</a>
-                  </p>
-                  <span>– James Branch</span>
-                </blockquote>
-              </div>
-            </div>  --><!-- end entry item --> 
-
-            <!-- video post 
-            <div class="entry-item">
-              <div class="entry-video video-wrap">
-                <iframe width="640" height="360" src="https://www.youtube.com/embed/1Yb6O79GKb4" allowfullscreen></iframe>
-              </div>
-
-              <div class="entry-date hidden-sm hidden-xs">
-                <span>11</span>
-                <span>MAR</span>                    
-              </div>
-
-              <div class="entry-title">
-                <h2>
-                  <a href="{{ url('berita') }}">VIDEO YOUTUBE : Nadiem Makarim, CEO & Founder Gojek Indonesia, Inspirasi sukses</a>
-                </h2>
-              </div>
-
-              <ul class="entry-meta">
-                <li class="entry-date">
-                  <a href="#">11 March, 2018</a>
-                </li>
-                <li class="entry-author">
-                  by <a href="#">Iqbal Buchori</a>
-                </li>
-                <li class="entry-category">
-                  in <a href="#">Programmer</a>
-                </li>
-                <li>
-                  <a href="{{ url('isiberita') }}" class="entry-comments">10 Comments</a>
-                </li>               
-              </ul>
-
-              <div class="entry">             
-                <div class="entry-content">
-                  <br>
-                  <a href="" class="read-more">Selengkapnya</a>
-                  <i class="icon arrow_right"></i> 
-                </div>
-              </div>
-            </div>  end entry item -->
 		
             <!-- Pagination -->
             <nav class="pagination clear text-center">
@@ -192,12 +79,6 @@
           
           <!-- sidebar -->
           <div class="col-sm-4 sidebar blog-sidebar">
-
-            <!-- <form role="form" class="relative">
-              <input type="search" class="searchbox" placeholder="Search">
-                <button type="submit" class="search-button"><i class="icon icon_search"></i></button>
-              </form>
- -->
               <!-- Categories -->
 
             <div class="widget latest-posts">
